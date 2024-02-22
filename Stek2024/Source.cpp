87f260afbba0da41b9e0a2bb1eed5a0407acee99
Sergey Makletsov <smaklets@kpfu.ru>
2024-02-22 10:32:58 +0300

    Шаблон

--- Stek2024/Source.cpp
+++ Stek2024/Source.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 void main()
 {
-	stek_elem* stek = nullptr;
+	stek_elem<double>* stek = nullptr;
 	for (int i = 1; i<=10; i++)
 	{
-		push(stek, i);
+		push(stek, i / 10.);
 	}
 	for (int i = 1; i <= 10; i++)
 	{
-		int k;
+		double k;
 		if (pop(stek, k))
 		{
 			cout << k << " ";
--- Stek2024/stek.h
+++ Stek2024/stek.h
@@ -1,11 +1,42 @@
 #pragma once
 
+template <typename T>
 struct stek_elem
 {
-	int x;
+	T x;
 	stek_elem* next;
 };
 
-void push(stek_elem*& stek, int value);
-bool pop(stek_elem*& stek, int& value);
-bool peek(stek_elem* stek, int& value);
+template <typename T>
+void push(stek_elem<T>*& stack_top, T value)
+{
+	auto* new_el = new stek_elem<T>;
+	new_el->x = value;
+	new_el->next = stack_top;
+	stack_top = new_el;
+}
+
+template <typename T>
+bool pop(stek_elem<T>*& stack_top, T& value)
+{
+	if (stack_top)
+	{
+		value = stack_top->x;
+		auto* del = stack_top;
+		stack_top = stack_top->next;
+		delete del;
+		return true;
+	}
+	return false;
+}
+
+template <typename T>
+bool peek(stek_elem<T>* stack_top, T& value)
+{
+	if (stack_top)
+	{
+		value = stack_top->x;
+		return true;
+	}
+	return false;
+}
